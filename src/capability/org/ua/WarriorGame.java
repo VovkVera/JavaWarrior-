229691e1de25ccaceca800c35b96f2cf70fca0d3
Vera Vovk <vovkvera@gmail.com>
2019-07-04 07:29:56 +0300

    Update WarriorGame.java

--- src/capability/org/ua/WarriorGame.java
+++ src/capability/org/ua/WarriorGame.java
@@ -4,10 +4,14 @@ public class WarriorGame {
 
 	public static void main(String[] args) throws InterruptedException {
 		Warrior thor = new Warrior("Thor", 800, 130, 40);
-		Warrior loki = new DodgeWarrior("Loki", 800, 85, 40, 0.25);
+		Warrior loki = new DodgeWarrior("Loki", 800, 85, 40, 1.25);
 		
 		Battle.startFight(thor, loki);
-
+		
+		System.out.println("Loki "+ loki.teleport());
+		loki.setTeleportAbility(new CanTeleport());
+		System.out.println("Loki "+ loki.teleport());
+		
 	}
 
 }
